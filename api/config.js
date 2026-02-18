679d576bbfbc9f480bf42589a554639b6652dbef
kim <eggkimkr@gmail.com>
2026-02-18 21:20:17 +0900

    진짜로 수정

--- api/config.js
+++ api/config.js
@@ -1,4 +1,9 @@
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
   if (req.method !== "GET") {
     return res.status(405).json({ error: "Method not allowed" });
   }
